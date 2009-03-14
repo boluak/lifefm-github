02d53bd10b40fec161dc690f686bbe4b2b1342bc
hmelyoff <hmelyoff@hmelyoffs-macbook-pro.local>
2009-03-14 20:39:53 +0300

    add joomla

---
+++ templates/template_sonia/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+<head>  
+<jdoc:include type="head" />  
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/template_sonia/css/template_css.css" type="text/css" />  
+</head>
+
+<body>
+
+<jdoc:include type="component" />
+<jdoc:include type="modules" name="left" />
+<jdoc:include type="modules" name="footer" />
+</body>
+</html>
