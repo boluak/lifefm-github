d58a0f8595e49b8490085f8ace15ce344af3e9e6
Soniajimenez <sonia@onixnoir.es>
2009-04-20 15:25:14 +0100

    change name of html to framework

--- html/javascripts/player.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-
-  var so = new SWFObject("./flash/volume_control.swf", "lifefmPlayer", "193", "193", "8");
-  so.addParam("scale", "noscale");
-  so.addParam("wmode", "transparent");
-  so.write("lifefmPlayer");
-  
-})
